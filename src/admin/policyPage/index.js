67dcd1f8f2299cabcfe7d5d66b26e56ef380b114
Veprev Alexey <Alexey.v.veprev@gmail.com>
2020-01-08 19:37:21 +0200

    fix jquery issue

--- src/admin/policyPage/index.js
+++ src/admin/policyPage/index.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 import App from "./components/App/App";
 
-jQuery(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function(){
     ReactDOM.render(
         <App />,
         document.getElementById('root')
